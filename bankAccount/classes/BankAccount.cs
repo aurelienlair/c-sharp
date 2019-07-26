ffb6b91dd10bc4080b8e0bca9adec2b64a081449
Aurélien Lair <aurelien.lair@gmail.com>
2019-07-26 10:43:24 +0200

    add bank account in c#

---
+++ bankAccount/classes/BankAccount.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        // Property has to be public to be visible from the outside
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                // `nameof` operator obtains the name of a variable, type, or member as the string constant:
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            report.AppendLine("\nDate\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+    }
+}
